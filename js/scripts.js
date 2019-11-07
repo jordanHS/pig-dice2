1fdc73e0ac695773fdca8c0207055f303e1e270d
Andrew Bates and Jordan Safford <andrew@>
2019-11-07 13:32:18 -0800

    add AI to program

--- js/scripts.js
+++ js/scripts.js
@@ -15,19 +15,111 @@ var playerOneScore = 0;
 var playerTwoScore = 0;
 var doubles = false;
 var compScore = 0;
+var cpuRolls = 0;
+var isCPU = false;
+
 
 PigDice.prototype.rollDie = function(min, max) {
   return Math.random() * (max - min);
 }
 
-PigDice.prototype.npcRoll = function() {
+NPC.prototype.hardRoll = function(){
+  //if turn score is < 20 then roll again
+  //when turn score is >= 20 hold
+  var cpuDieOne = 0;
+  var cpuDieTwo = 0;
+  var tempCompVal = 0;
+  var notBusted = true;
+  var doubles = false;
+  while ((tempCompVal <= 20 && notBusted) || doubles) {
+    console.log("inloop");
+    cpuDieOne = Math.ceil(dice.rollDie(1,7));
+    cpuDieTwo = Math.ceil(dice.rollDie(1,7));
+    doubles = false;
+    var cpuRollTotal = (cpuDieOne + cpuDieTwo);
+    if (cpuDieOne === 1 && cpuDieTwo === 1) {
+      compScore = 0;
+      tempCompVal = 0;
+      notBusted = false;
+      $("#currentScore").append("Cpu rolled Snake Eyes.<br>");
+      $("#playerTwoResults").html("0");
+    } else if (cpuDieOne === 1 || cpuDieTwo === 1) {
+      tempCompVal = 0;
+      notBusted = false;
+      $("#currentScore").append("Cpu rolled a one. " + cpuDieOne + ", " + cpuDieTwo + "<br>");
+    } else if (cpuDieOne === cpuDieTwo) {
+      tempCompVal += cpuRollTotal;
+      doubles = true;
+      $("#currentScore").append("Cpu rolled Doubles. " + cpuDieOne + ", " + cpuDieTwo + "<br>");
+    } else {
+      tempCompVal += cpuRollTotal;
+      $("#currentScore").append("Cpu rolled " + cpuDieOne + ", " + cpuDieTwo + "<br>");
+    }
+  }
+  if (tempCompVal > 20 && notBusted) {
+    npc.npcHold(tempCompVal);
+  }
+}
 
+NPC.prototype.npcRoll = function(diff) {
+  var cpuRolls = 0;
+  var cpuHadOne = false;
+  $("#currentPlayer").html(name2 + ": ")
+
+  if (diff === "easy") {
+    var cpuDieOne = 0;
+    var cpuDieTwo = 0;
+    var tempCompVal = 0;
+    setTimeout(function(){
+      while (cpuRolls < 2) {
+        cpuDieOne = Math.ceil(dice.rollDie(1,7));
+        cpuDieTwo = Math.ceil(dice.rollDie(1,7));
+        var cpuRollTotal = (cpuDieOne + cpuDieTwo);
+        if (cpuDieOne === 1 && cpuDieTwo === 1) {
+          cpuRolls += 2;
+          compScore = 0;
+          tempCompVal = 0;
+          cpuHadOne = true;
+          $("#currentScore").append("Cpu rolled Snake Eyes.<br>");
+          $("#playerTwoResults").html("0");
+        } else if (cpuDieOne === 1 || cpuDieTwo === 1) {
+          cpuRolls += 2;
+          tempCompVal = 0;
+          cpuHadOne = true;
+          $("#currentScore").append("Cpu rolled a one. " + cpuDieOne + ", " + cpuDieTwo + "<br>");
+        } else if (cpuDieOne === cpuDieTwo) {
+          tempCompVal += cpuRollTotal;
+          $("#currentScore").append("Cpu rolled Doubles. " + cpuDieOne + ", " + cpuDieTwo + "<br>");
+        } else {
+          cpuRolls++;
+          tempCompVal += cpuRollTotal;
+          $("#currentScore").append("Cpu rolled " + cpuDieOne + ", " + cpuDieTwo + "<br>");
+        }
+      }
+      if (!cpuHadOne) {
+        npc.npcHold(tempCompVal);
+      }
+    }, 250);
+  } else if (diff === "hard") {
+    console.log("hard");
+    npc.hardRoll();
+  }
+}
+
+NPC.prototype.npcHold = function(turnVal){
+  compScore += turnVal;
+  player = true;
+  if (compScore >= 100) {
+    $(".results").html("<p>" + name2 + " Wins!</p><br>");
+    $("button#reset").show();
+  }
+  $("#playerTwoResults").html(" " + compScore);
 }
 
 PigDice.prototype.roll = function(){
   var dieOne = Math.ceil(dice.rollDie(1, 7));
   var dieTwo = Math.ceil(dice.rollDie(1, 7));
-  console.log(dieOne, dieTwo);
+  $("#cpuResults").html("");
   if (doubles) {
     $("button#hold").prop("disabled", false);
     doubles = false;
@@ -36,28 +128,41 @@ PigDice.prototype.roll = function(){
     tempScore = 0;
     if (player) {
       playerOneScore = 0;
+      $("#currentScore").html("");
       $("#playerOneResults").html(" 0");
     } else if (!player) {
       playerTwoScore = 0;
+      $("#currentScore").html("");
       $("#playerTwoResults").html(" 0");
     }
-      player = !player;
-      $("#rolledResult").html("snake eyes and lost all your points!");
+    player = !player;
+    $("#rolledResult").html("snake eyes and lost all your points!");
+    if (isCPU) {
+      player = true;
+      npc.npcRoll(npc.difficulty);
+    }
   } else if (dieOne === 1 || dieTwo === 1) {
     tempScore = 0;
     player = !player;
     $("#rolledResult").html("1!");
+    $("#currentScore").html("");
+    if (isCPU) {
+      player = true;
+      npc.npcRoll(npc.difficulty);
+    }
   } else if (dieOne === dieTwo) {
     tempScore += (dieOne + dieTwo);
     $("currentScore").html();
     $("#rolledResult").html("doubles, " + dieOne + " and " + dieTwo + " Roll again!");
     $("button#hold").prop("disabled", true);
     doubles = true;
+    return tempScore;
   } else {
     tempScore += (dieOne + dieTwo);
-    $("#rolledResult").html((dieOne.toString()) + " and " + dieTwo.toString())
+    $("#rolledResult").html((dieOne.toString()) + " and " + dieTwo.toString());
+    return tempScore;
   }
-  return tempScore;
+  // return tempScore;
 }
 
 PigDice.prototype.hold = function(){
@@ -70,6 +175,10 @@ PigDice.prototype.hold = function(){
       $(".results").html("<p>" + name1 + " Wins!</p><br>");
       $("button#reset").show();
     } else {
+      if (isCPU) {
+        player = true;
+        npc.npcRoll(npc.difficulty);
+      }
       return playerOneScore;
     }
   } else if (!player) {
@@ -101,22 +210,30 @@ $(document).ready(function(event){
   $("button#computer").click(function(){
     $("#difficulty").show();
     $("#gameMode").hide();
+    isCPU = true;
   });
 
-  $("#easy, #hard").click(function(){
-    npc.difficulty = true;
+  $("#easy").click(function(){
+    npc.difficulty = "easy";
     $("#onePlayer").show();
     $("#difficulty").hide();
   });
 
-  //need to differentiate between two players and one vs computer.
-  //add variable to flag cpu as opponent
-  //use cpu flag to block button on cpu's return and name the second player.
-  //have the computers moves displayed below, ie cpu rolled a 5, cpu rolled a 4, cpu held.
+  $("#hard").click(function(){
+    npc.difficulty = "hard";
+    $("#onePlayer").show();
+    $("#difficulty").hide();
+  });
 
   $("#onePlayerName").click(function(){
-    dice.name = $("#onePlayer").val();
+    dice.name = $("#playerName").val();
+    name1 = dice.name;
+    name2 = "Computer"
+    $("#scoreName1").html(name1);
+    $("#scoreName2").html(name2);
     $("#resultsBlock").show();
+    $("div#playButtons").show();
+    $("#onePlayer").hide();
   });
 
   $("button#names").click(function(event){
